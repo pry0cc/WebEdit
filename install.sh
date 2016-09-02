a602ed077692f8a4b96d35ef470b1a62fcd80891
pry0cc <pry0cc@protonmail.com>
2016-09-02 01:26:51 +0000

    Added install/uninstall

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo mkdir /usr/share/webedit
+sudo cp -r * /usr/share/webedit/
+sudo ln -s /usr/share/webedit/webedit /usr/bin/webedit
---
+++ uninstall.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo rm -r /usr/share/webedit/
+sudo rm -r /usr/bin/webedit
